5745b7b5546c709ccdb8ef1fa4a451ee50f1e755
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2023-11-16 22:41:31 -0600

    Add files and solution for day 5 part 2

---
+++ day05_part2/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+)
+
+type Node struct {
+	unit rune
+	next *Node
+	prev *Node
+}
+
+func parse(filename string) string {
+	data, err := os.ReadFile(filename)
+
+	if err != nil {
+		panic("file error")
+	}
+	content := string(data)
+
+	return strings.Trim(content, "\n")
+}
+
+func reduce(polymer *Node) bool {
+	current := polymer
+	removed := false
+
+	for current != nil && current.next != nil {
+		current_char := current.unit
+		next_char := current.next.unit
+
+		// handle of complex logic
+		units_are_same_type := unicode.ToLower(current_char) == unicode.ToLower(next_char)
+		first_unit_low_and_second_upper := unicode.IsLower(current_char) && unicode.IsUpper(next_char)
+		first_unit_upper_and_second_low := unicode.IsUpper(current_char) && unicode.IsLower(next_char)
+		units_are_opposite_polarity := first_unit_low_and_second_upper || first_unit_upper_and_second_low
+
+		if units_are_same_type && units_are_opposite_polarity {
+			// get relevant nodes for removal
+			previous := current.prev
+			next := current.next.next
+
+			// remove both nodes
+			previous.next = next
+			next.prev = previous
+
+			removed = true
+			current = previous
+		} else {
+			current = current.next
+		}
+	}
+
+	return removed
+}
+
+func create_list(polymer string, exclude_char rune) *Node {
+	dummy_head := &Node{unit: '>', next: nil, prev: nil}
+	current := dummy_head
+
+	for _, char := range polymer {
+		if char == exclude_char || char == unicode.ToUpper(exclude_char) {
+			continue
+		}
+		node := &Node{unit: char, next: nil, prev: current}
+		current.next = node
+		current = current.next
+	}
+	dummy_tail := &Node{unit: '<', next: nil, prev: current}
+	current.next = dummy_tail
+
+	return dummy_head
+}
+
+func polymer_len(polymer *Node) int {
+	length := 0
+	current := polymer
+
+	for current != nil {
+		length += 1
+		current = current.next
+	}
+	return length - 2
+}
+
+func solve(polymer *Node) int {
+
+	removed := reduce(polymer)
+	for removed {
+		removed = reduce(polymer)
+	}
+
+	return polymer_len(polymer)
+}
+
+func solution(filename string) int {
+	polymer := parse(filename)
+
+	min_length := len(polymer)
+	for letter := 'a'; letter <= 'z'; letter++ {
+		polymer_list := create_list(polymer, letter)
+		length := solve(polymer_list)
+		if length < min_length {
+			min_length = length
+		}
+	}
+	return min_length
+}
+
+func main() {
+	fmt.Println(solution("./input.txt"))
+}
