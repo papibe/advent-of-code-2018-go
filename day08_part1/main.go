e330e1d4b789f88687fd6d8d817e4e6f8f8d9254
Pablo Piaggio <pabpia@gmail.com>
2023-11-23 22:22:07 -0600

    Add working solution for day 8 part 1

--- day08_part1/main.go
+++ day08_part1/main.go
@@ -25,54 +25,36 @@ func parse(filename string) []int {
 func solve(lf []int, start int, nodes int) (int, int) {
 	internal_nodes := lf[start]
 	meta_size := lf[start+1]
-	// fmt.Println("index: {}, ", start, internal_nodes, meta_size)
-	fmt.Println("index:", start, "nodes:", nodes)
 
-	// if internal_nodes == 0 {
-	// 	meta_sum := 0
-	// 	for i := 0; i < meta_size; i++ {
-	// 		meta_sum += lf[start+2+i]
-	// 	}
-	// 	return meta_sum, start + 1 + meta_size
-	// }
+	if internal_nodes == 0 {
+		meta_sum := 0
+		for i := 0; i < meta_size; i++ {
+			meta_sum += lf[start+2+i]
+		}
+		return meta_sum, start + 1 + meta_size
+	}
 
 	current_sum := 0
 	new_start := start + 2
-	for i := 0; i < nodes; i++ {
-		fmt.Println("cycle")
-
-		if lf[new_start] == 0 {
-			fmt.Println("\tzero case")
-
-			meta_sum := 0
-			for i := 0; i < lf[new_start+1]; i++ {
-				meta_sum += lf[new_start+2+i]
-			}
-			// return meta_sum, start + 1 + meta_size
-			current_sum += meta_sum
-			new_start = new_start + 1 + lf[new_start+1]
-		} else {
-			fmt.Println("\tdeep")
-			sum, end := solve(lf, new_start, internal_nodes)
-			current_sum += sum
-			new_start = end + 1
-		}
+	for i := 0; i < internal_nodes; i++ {
+		sum, end := solve(lf, new_start, 1)
+		current_sum += sum
+		new_start = end + 1
 
 	}
 	meta_sum := 0
 	for i := 0; i < meta_size; i++ {
 		meta_sum += lf[new_start+i]
 	}
-
-	return current_sum + meta_sum, new_start + meta_size
+	return current_sum + meta_sum, new_start + meta_size - 1
 }
 
 func solution(filename string) int {
 	licence := parse(filename)
-	sum, _ := solve(licence, 0, 2)
+	sum, _ := solve(licence, 0, 1)
 	return sum
 }
 
 func main() {
-	fmt.Println(solution("./example.txt"))
+	fmt.Println(solution("./input.txt"))
 }
