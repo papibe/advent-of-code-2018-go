3381d2cb53f9b50ece184194dd7825b7421bc397
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2023-11-27 19:54:51 -0600

    Add solution and files for day 8 part 1

---
+++ day08_part1/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func parse(filename string) []int {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		panic("File not found")
+	}
+	content := string(data)
+
+	numbers := []int{}
+	for _, str_number := range strings.Split(strings.Trim(content, "\n"), " ") {
+		number, _ := strconv.Atoi(str_number)
+		numbers = append(numbers, number)
+	}
+	return numbers
+}
+
+func solve(lf []int, start int) (int, int) {
+	internal_nodes := lf[start]
+	meta_size := lf[start+1]
+
+	if internal_nodes == 0 {
+		meta_sum := 0
+		for i := 0; i < meta_size; i++ {
+			meta_sum += lf[start+2+i]
+		}
+		return meta_sum, start + 1 + meta_size
+	}
+
+	current_sum := 0
+	new_start := start + 2
+	for i := 0; i < internal_nodes; i++ {
+		sum, end := solve(lf, new_start)
+		current_sum += sum
+		new_start = end + 1
+
+	}
+	meta_sum := 0
+	for i := 0; i < meta_size; i++ {
+		meta_sum += lf[new_start+i]
+	}
+	return current_sum + meta_sum, new_start + meta_size - 1
+}
+
+func solution(filename string) int {
+	license := parse(filename)
+	sum, _ := solve(license, 0)
+	return sum
+}
+
+func main() {
+	fmt.Println(solution("./input.txt")) // 48443
+}
