2d46c03e4109b58939d6019d68588e78eb47aabe
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2024-04-09 22:10:20 -0500

    Update day23 part 2

--- day23_part2/main.go
+++ day23_part2/main.go
@@ -5,7 +5,6 @@ import (
 	"math"
 	"os"
 	"regexp"
-	"sort"
 	"strconv"
 	"strings"
 )
@@ -15,12 +14,6 @@ type Nanobot struct {
 	y      int
 	z      int
 	radius int
-}
-
-type Coord struct {
-	x int
-	y int
-	z int
 }
 
 func parse(filename string) []Nanobot {
@@ -45,223 +38,132 @@ func parse(filename string) []Nanobot {
 	return nanobots
 }
 
-func get_points_inside(nanobot Nanobot) map[Coord]bool {
-	x0 := nanobot.x
-	y0 := nanobot.y
-	z0 := nanobot.z
-	r := nanobot.radius
-
-	points := make(map[Coord]bool)
-	for x := x0; x <= x0+r; x++ {
-		max_y := y0 + r + x0 - x
-
-		for y := y0; y <= max_y; y++ {
-			d := int(float64(r) - math.Abs(float64(x0)-float64(x)) - math.Abs(float64(y0)-float64(y)))
-
-			for z := z0 - d; z <= z0+d; z++ {
-				points[Coord{x, y, z}] = true
-			}
-
-			d = int(float64(r) - math.Abs(float64(x0)-float64(x)) - math.Abs(float64(y0)-float64(y0-(y-y0))))
-			for z := z0 - d; z <= z0+d; z++ {
-				points[Coord{x, y0 - (y - y0), z}] = true
-			}
-
-			d = int(float64(r) - math.Abs(float64(x0)-float64(x0-(x-x0))) - math.Abs(float64(y0)-float64(y)))
-			for z := z0 - d; z <= z0+d; z++ {
-				points[Coord{x0 - (x - x0), y, z}] = true
-			}
-
-			d = int(float64(r) - math.Abs(float64(x0)-float64(x0-(x-x0))) - math.Abs(float64(y0)-float64(y0-(y-y0))))
-			for z := z0 - d; z <= z0+d; z++ {
-				points[Coord{x0 - (x - x0), y0 - (y - y0), z}] = true
-			}
-
+func BronKerbosch(r, p, x map[int]bool, graph []map[int]bool, result *[][]int) {
+	if len(p) == 0 && len(x) == 0 {
+		clique := []int{}
+		for v := range r {
+			clique = append(clique, v)
+		}
+		*result = append(*result, clique)
+		return
+	}
+	// pivoting
+	var u int
+	if len(p) > 0 {
+		for v := range p {
+			u = v
+			break
+		}
+	} else {
+		for v := range x {
+			u = v
+			break
 		}
 	}
-	return points
-}
-
-func distance(point Coord, nanobot Nanobot) int {
-	d := math.Abs(float64(nanobot.x)-float64(point.x)) + math.Abs(float64(nanobot.y)-float64(point.y)) + math.Abs(float64(nanobot.z)-float64(point.z))
-
-	return int(d)
-}
-
-func ndistance(nb1, nb2 Nanobot) int {
-	d := math.Abs(float64(nb1.x)-float64(nb2.x)) + math.Abs(float64(nb1.y)-float64(nb2.y)) + math.Abs(float64(nb1.z)-float64(nb2.z))
-
-	return int(d)
-}
-
-func is_inside(nb Nanobot, x, y, z int) bool {
-	d := int(math.Abs(float64(nb.x)-float64(x)) + math.Abs(float64(nb.y)-float64(y)) + math.Abs(float64(nb.z)-float64(z)))
-	return d <= nb.radius
-}
-
-func sort_points_by_intersections(points []Coord, intersections map[Coord]int) {
-	sort.Slice(points, func(i, j int) bool {
-		return intersections[points[i]] > intersections[points[j]]
-	})
-}
-
-func get_intersections(point Coord, nanobots []Nanobot) int {
-	counter := 0
-	for j := 0; j < len(nanobots); j++ {
-		if is_inside(nanobots[j], point.x, point.y, point.z) {
-			counter += 1
+	p_minus_nu := make(map[int]bool)
+	for v := range p {
+		_, is_connected_to_v := graph[u][v]
+		if !is_connected_to_v {
+			p_minus_nu[v] = true
 		}
 	}
-	return counter
-}
-
-func man_to_origin(next_point Coord) int {
-	return int(math.Abs(float64(next_point.x)) + math.Abs(float64(next_point.y)) + math.Abs(float64(next_point.z)))
-}
-
-func bfs(
-	point Coord,
-	visited *map[Coord]bool,
-	current_intersections, max_man_distance, max_bfs_rec int,
-	nanobots []Nanobot,
-) (int, int, Coord) {
-	queue := []Coord{point}
-	(*visited)[point] = true
-	local_min_man := math.MaxInt
-	min_point := point
-
-	for len(queue) > 0 && max_bfs_rec > 0 {
-		p := queue[0]
-		x, y, z := p.x, p.y, p.z
-		queue = queue[1:]
-		next_points := []Coord{
-			{x, y, z + 1},
-			{x, y, z - 1},
-
-			{x, y + 1, z},
-			{x, y - 1, z},
-
-			{x + 1, y, z},
-			{x - 1, y, z},
+	for v := range p_minus_nu {
+		new_r := make(map[int]bool)
+		for key, value := range r {
+			new_r[key] = value
 		}
-		for _, next_point := range next_points {
-			_, is_visited := (*visited)[next_point]
-			if is_visited {
+		new_r[v] = true
+
+		new_p := make(map[int]bool)
+		for key, value := range p {
+			_, is_connected_to_v := graph[v][key]
+			if is_connected_to_v {
+				new_p[key] = value
+			}
+		}
+
+		new_x := make(map[int]bool)
+		for key, value := range x {
+			_, is_connected_to_v := graph[v][key]
+			if is_connected_to_v {
+				new_x[key] = value
+			}
+		}
+
+		BronKerbosch(new_r, new_p, new_x, graph, result)
+		delete(p, v)
+		x[v] = true
+	}
+
+}
+
+func get_max_cliques(graph []map[int]bool) []int {
+	r := make(map[int]bool)
+	x := make(map[int]bool)
+
+	p := make(map[int]bool)
+	for i := 0; i < len(graph); i++ {
+		p[i] = true
+	}
+	cliques := [][]int{}
+	BronKerbosch(r, p, x, graph, &cliques)
+	// get bigest cliques
+	max_len := math.MinInt
+	max_index := -1
+	for index, clique := range cliques {
+		if len(clique) > max_len {
+			max_len = len(clique)
+			max_index = index
+		}
+	}
+	return cliques[max_index]
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func intersects(n1, n2 Nanobot) bool {
+	distance := abs(n1.x-n2.x) + abs(n1.y-n2.y) + abs(n1.z-n2.z)
+	return distance <= n1.radius+n2.radius
+}
+
+func form_graph(nanobots []Nanobot) []map[int]bool {
+	graph := []map[int]bool{}
+	for i, nbot1 := range nanobots {
+		connections := make(map[int]bool)
+		for j, nbot2 := range nanobots {
+			if i == j {
 				continue
 			}
-			intersections := get_intersections(next_point, nanobots)
-			if intersections >= current_intersections {
-				current_intersections = intersections
-				current_man := man_to_origin(next_point)
-				// fmt.Println("    ", next_point, current_intersections, current_man)
-				// local_min_man = min(local_min_man, current_man)
-				if current_man < local_min_man {
-					local_min_man = current_man
-					min_point = next_point
-				}
+			if intersects(nbot1, nbot2) {
+				connections[j] = true
 			}
-
-			(*visited)[next_point] = true
-			queue = append(queue, next_point)
-			// fmt.Println("    ", next_point, current_intersections, local_max_man)
 		}
-
-		max_bfs_rec--
+		graph = append(graph, connections)
 	}
-	return local_min_man, current_intersections, min_point
-}
-
-func solve(nanobots []Nanobot) int {
-	// var best_coord = Coord{0, 0, 0}
-	// max_num_nanobots := math.MinInt
-
-	max_intersections := math.MinInt
-	intersections := make(map[Coord]int)
-	for i := 0; i < len(nanobots); i++ {
-		corners := [][]int{
-			{nanobots[i].x, nanobots[i].y, nanobots[i].z + nanobots[i].radius},
-			{nanobots[i].x, nanobots[i].y, nanobots[i].z - nanobots[i].radius},
-
-			{nanobots[i].x, nanobots[i].y + nanobots[i].radius, nanobots[i].z},
-			{nanobots[i].x, nanobots[i].y - nanobots[i].radius, nanobots[i].z},
-
-			{nanobots[i].x + nanobots[i].radius, nanobots[i].y, nanobots[i].z},
-			{nanobots[i].x - nanobots[i].radius, nanobots[i].y, nanobots[i].z},
-
-			{nanobots[i].x, nanobots[i].y, nanobots[i].z},
-		}
-
-		for _, corner := range corners {
-			counter := 0
-			for j := 0; j < len(nanobots); j++ {
-				if is_inside(nanobots[j], corner[0], corner[1], corner[2]) {
-					counter += 1
-				}
-			}
-			value, seen_point_before := intersections[Coord{corner[0], corner[1], corner[2]}]
-			if seen_point_before {
-				fmt.Println("---------><--------------", value, counter)
-			}
-			intersections[Coord{corner[0], corner[1], corner[2]}] = counter
-			max_intersections = max(max_intersections, counter)
-		}
-	}
-
-	points := []Coord{}
-	for point, _ := range intersections {
-		points = append(points, point)
-	}
-	sort_points_by_intersections(points, intersections)
-
-	//
-	// TODO: take every point and do BFS
-	// each point shound be equal or better intersections
-	// for each point calculate distance to origin
-
-	max_bfs_rec := 5_500_000
-	// max_bfs_rec := 1_000
-	max_list_proc := 1
-	// max_list_proc := 6000
-	min_man_distance := man_to_origin(points[0])
-	max_intersections = intersections[points[0]]
-	visited := make(map[Coord]bool)
-
-	fmt.Println("max inter", max_intersections)
-	fmt.Println("max man", man_to_origin(points[0]))
-
-	for i := 0; i < max_list_proc; i++ {
-		fmt.Println(points[i], intersections[points[i]])
-		// visited := make(map[Coord]bool)
-
-		local_min_man, current_intersections, min_point := bfs(
-			points[i],
-			&visited,
-			// max_intersections,
-			intersections[points[i]],
-			min_man_distance,
-			max_bfs_rec,
-			nanobots,
-		)
-		if current_intersections >= max_intersections {
-			max_intersections = current_intersections
-			min_man_distance = min(min_man_distance, local_min_man)
-			fmt.Println("--->", current_intersections, min_man_distance, min_point, len(visited))
-		} else {
-			fmt.Println("--><", current_intersections, min_man_distance, len(visited))
-		}
-	}
-
-	return min_man_distance
+	return graph
 }
 
 func solution(filename string) int {
 	nanobots := parse(filename)
-	// fmt.Println("num nano", len(nanobots))
-	return solve(nanobots)
+	graph := form_graph(nanobots)
+	clique := get_max_cliques(graph)
+
+	max_distance := math.MinInt
+	for _, bot_index := range clique {
+		nanobot := nanobots[bot_index]
+		manhatan := abs(nanobot.x) + abs(nanobot.y) + abs(nanobot.z)
+		distance := manhatan - nanobot.radius
+		max_distance = max(max_distance, distance)
+	}
+
+	return max_distance
 }
 
 func main() {
-	// fmt.Println(solution("./example.txt")) // 36
-	fmt.Println(solution("./input.txt")) //
-	// 82444530 too high
+	fmt.Println(solution("./input.txt")) // 78687716
 }
